91176979fb920b9892705a3cb8ae2beb197b4f7e
Saanjh Khattar <49347021+saanjhkhattar2@users.noreply.github.com>
2019-07-29 13:55:09 -0400

    Add files via upload

---
+++ day1code.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file. 
+"""
+#print ("Hello my name is Saanjh")
+#print ("Spyder")
+
+def multiply(a,b):
+    multiply = a*b
+    
+    print (multiply)
+    
+multiply(4,5)
+
+def sum(a,b):
+    sum = a+b
+    
+    print (sum)
+    
+sum(4,5)
+
+def area(b,h):
+    area = (.5)*b*h
+    
+    print("The area of the triangle with base", b,"and height", h, "=", area) 
+
+    
+area(3,5)
+
